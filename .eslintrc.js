107ff2dec510de8f4dfa4db1d3140749fe3c505b
Sahal Imran <sahalimran.pro@gmail.com>
2024-05-09 19:10:02 +0530

    Header Component Done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
+const { error } = require("console");
+
 module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules:{
+    'prettier/prettier': ['error',{endOfLine:'auto'}],
+    'no-console':'off',
+  }
 }
